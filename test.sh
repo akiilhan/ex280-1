67c756a7fa9c9e7042c9822991853857471cc071
ramanathan <ramanathanrobotist@gmail.com>
2023-04-25 23:54:11 +0530

    intent error rectified

--- test.sh
+++ test.sh
@@ -1,34 +1,5 @@
-#!/bin/bash
-
-#copy kubeadmin password and api to workstation
-scp -r root@utility:/home/lab/ocp4/auth/kube* .
-x=$(cat /home/student/ex280/kubeadmin-password)
-y=$(cat /home/student/ex280/kubeconfig | grep server | awk -F" " '{print $2}'|uniq) 
-
-#since server is slow it will take some time connect
-sleep 300;
-
-#login kubeadmin untill its logged
-oc login -u kubeadmin -p $x $y
-while [ "$(oc whoami)" != "kube:admin" ]
-
-do
-   echo  "kubeadmin not logged";
-   echo  "kubeadmin  logging";
-   oc login -u kubeadmin -p $x $y;
-   sleep 60;
-
-done
-
-#more waiting time
-sleep 200;
-
-
 #verify the kubeadmin again
 oc login -u kubeadmin -p $x $y
-
-oc adm taint node master02 key1=value1:NoSchedule
-oc adm taint node master03 key1=value1:NoSchedule
 
 for i in bullwinkle,rocky gru,minion lerna,hydra area51,oxcart math,qed apples,oranges pathfinder,voyager mercury,atlas; do 
     IFS=',' read project_name item2 <<< "${i}"
@@ -51,7 +22,7 @@ for i in bullwinkle,rocky gru,minion lerna,hydra area51,oxcart math,qed apples,o
         oc label node master02 start=trick
         oc label node master03 start=trick
     fi
-    
+
     echo "$project_name content" > index.html && oc new-app --name=$app_name  --image=nginx --strategy=source --binary=true --output=yaml | oc apply -f - && oc start-build $app_name --from-dir=./ --follow
 
     oc expose service $app_name
@@ -59,3 +30,4 @@ done
 
 
 oc project default
+
