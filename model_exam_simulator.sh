62c6acc85afb0afdc58fa824cc9787df679903cd
ramanathan <ramanathanrobotist@gmail.com>
2023-04-26 00:56:27 +0530

    changed the image back to httpd

--- model_exam_simulator.sh
+++ model_exam_simulator.sh
@@ -47,13 +47,13 @@ for i in bullwinkle,rocky gru,minion lerna,hydra area51,oxcart math,qed apples,o
 
     done
 
-    if [ "$(oc project -q)" != "pathfinder" ]; then 
+    if [ "$(oc project -q)" == "pathfinder" ]; then 
         oc label node master01 start=trick
         oc label node master02 start=trick
         oc label node master03 start=trick
     fi
     
-    echo "$project_name content" > index.html && oc new-app --name=$app_name  --image=nginx --strategy=source --binary=true --output=yaml | oc apply -f - && oc start-build $app_name --from-dir=./ --follow
+    echo "$project_name content" > index.html && oc new-app --name=$app_name  --image=httpd --strategy=source --binary=true --output=yaml | oc apply -f - && oc start-build $app_name --from-dir=./ --follow
 
     oc expose service $app_name
 done
